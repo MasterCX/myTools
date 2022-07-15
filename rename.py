3b8b4a0386e7a8b2b3e13362ad1de1ac923a5420
Zhuoqun <lnhldwzq@gmail.com>
2022-07-15 09:38:22 +0800

    rename.py updated

--- rename.py
+++ rename.py
@@ -1,22 +1,25 @@
 import os
+from sqlalchemy import except_all
 
 from sympy import arg
 
 def rename():
-    path = 'D:\project\wooden_board\木材照片（结疤）'
+    path = 'D:\project\wooden_board\pos_samples'
     format = '.png'
     filelist = os.listdir(path)
     total_num = len(filelist)
     i = 0
     for item in filelist:
+        
         if item.endswith(format):
             src = os.path.join(os.path.abspath(path), item)
             dst = os.path.join(os.path.abspath(path), '' + str(i) + format )
+            print(dst)
             try:
                 os.rename(src, dst)
                 i += 1
-            except:
-                continue
+            except BaseException as e:
+                i += 1
     print('total %d to rename & converted %d png'%(total_num, i))
 
 if __name__ == '__main__':
