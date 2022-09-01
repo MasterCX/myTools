1d41db3256cfa9923e9b9d6c899c6954e2d22b42
Zhuoqun <lnhldwzq@gmail.com>
2022-09-01 17:00:44 +0800

    division

--- utils/division.py
+++ utils/division.py
@@ -1,11 +1,20 @@
 import cv2
+import os
 
 
-def divide(img, numX, numY):
-    w, h, c = img.shape
-    
+def divide(path, fileName, outputPath):
+    img = cv2.imread(path + '/' + fileName, cv2.IMREAD_UNCHANGED)
+    h, w, c = img.shape
+    half = int(w/2)
+    img1 = img[:, 0:half]
+    img2 = img[:, half:]
+    cv2.imwrite(outputPath + '/1_' + fileName, img1)
+    cv2.imwrite(outputPath + '/2_' + fileName, img2)
 
 
 if __name__ == '__main__':
-    img = cv2.imread('img/Image_20220815104036839.jpg', cv2.IMREAD_UNCHANGED)
-    divide(img, 4, 3)
+    path = '../img_output'
+    outputPath = '../img_others'
+    fileList = os.listdir(path)
+    for fileName in fileList:
+        divide(path, fileName, outputPath)
--- utils/resize.py
+++ utils/resize.py
@@ -5,20 +5,20 @@ import os
 def resize(path, outputPath, xR, yR, fileName):
     img = cv2.imread(path + '/' + fileName, cv2.IMREAD_UNCHANGED)
     imgShape = img.shape
-    # newH = int(imgShape[0]*xR)
-    # newW = int(imgShape[1]*yR)
-    newH = 1280
-    newW = 1280
+    newH = int(imgShape[0]*yR)
+    newW = int(imgShape[1]*xR)
+    # newH = 1280
+    # newW = 1280
     newImg = cv2.resize(img, (newW, newH))
     cv2.imwrite(outputPath + '/' + fileName, newImg)
 
 
 if __name__ == '__main__':
-    path = 'img'
-    outputPath = 'img_output'
+    path = '../images/all'
+    outputPath = '../img_output'
     # 在xy轴上的系数
     xR = 0.5
-    yR = 0.5
+    yR = 1.0
     fileList = os.listdir(path)
     for fileName in fileList:
         resize(path, outputPath, xR, yR, fileName)
