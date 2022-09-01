ef75f2ff2fee39f97c72ceb417ad2e6a44419e72
wzq <lnhldwzq@gmail.com>
2022-09-01 16:43:37 +0800

    change name

--- utils/random_select_files.py
+++ utils/random_select_files.py
@@ -5,7 +5,7 @@ import shutil
 
 # 随机拷贝指定文件夹内的文件到其他文件夹。
 
-# 如果文件夹内的文件数量少于设置的拷贝文件数量，则会全部拷贝。
+# 如果文件夹内的文件数量少于设置的拷贝文件数量，取80%
 # 如果目的文件夹不存在，会新建文件夹。
 # 只会拷贝文件，不会拷贝子文件夹。
 
@@ -17,9 +17,12 @@ def newDir(fileDir):
         print(fileDir+'already exists!!!')
 
 
+
 def randomCopyFile(sourDir, dstDir, num):
     pathDir = os.listdir(sourDir)
     newDir(dstDir)
+    if len(pathDir) < num:
+        num = int(len(pathDir) * 0.8)
     samples = random.sample(pathDir, num)
     for sample in samples:
         print(sample)
@@ -28,7 +31,7 @@ def randomCopyFile(sourDir, dstDir, num):
 
 
 if __name__ == '__main__':
-    source = "/home/w/data/folder/"
-    dst = "/home/x/data/folder/"
-    num = 500
+    source = "../../wood/images/s/"
+    dst = "./img/"
+    num = 600
     randomCopyFile(source, dst, num)
